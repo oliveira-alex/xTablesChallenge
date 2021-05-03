906a73bceef6a8707a11cb3835b6993889447f01
ALeX <alexdeoliveira.poli@gmail.com>
2021-05-03 15:13:34 -0300

    Sketch of settings view

--- xTablesChallenge/ContentView.swift
+++ xTablesChallenge/ContentView.swift
@@ -9,8 +9,65 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        Text("Hello, world!")
-            .padding()
+        NavigationView {
+            VStack(spacing: 20) {
+                Spacer()
+                
+                VStack {
+                    Text("Which tables do you want to practice?")
+
+                    HStack {
+                        ForEach(1 ..< 5) {
+                            Button("\($0)") {
+                                //
+                            }
+                        }
+                    }
+
+                    HStack {
+                        ForEach(5 ..< 9) {
+                            Button("\($0)") {
+                                //
+                            }
+                        }
+                    }
+
+                    HStack {
+                        ForEach(9 ..< 13) {
+                            Button("\($0)") {
+                                //
+                            }
+                        }
+                    }
+                }
+                
+                VStack {
+                    Text("How many questions do you want to be asked?")
+                    
+                    HStack {
+                        ForEach(0 ..< 3) {
+                            let buttonTitle = 5.0 * pow(2.0, Double($0))
+                            Button("\(Int(buttonTitle))") {
+                                //
+                            }
+                        }
+                        
+                        Button("All") {
+                            //
+                        }
+                    }
+                }
+                
+                Spacer()
+                
+                Button("Start") {
+                    //
+                }
+                
+                Spacer()
+            }
+            .navigationBarTitle("xTables")
+        }
     }
 }
 

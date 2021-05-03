0ca4b417e94639e9913d49f9b5385c7b8cdb8741
ALeX <alexdeoliveira.poli@gmail.com>
2021-05-03 13:44:58 -0300

    Initial Commit

---
+++ xTablesChallenge/xTablesChallengeApp.swift
@@ -0,0 +1,17 @@
+//
+//  xTablesChallengeApp.swift
+//  xTablesChallenge
+//
+//  Created by Alex Oliveira on 03/05/2021.
+//
+
+import SwiftUI
+
+@main
+struct xTablesChallengeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
